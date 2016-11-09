b2443f67d447069c77b753b263c62342ed5c7e32
ssfxate <vadimnechaev@MacBook-Vadima.local>
2016-11-09 19:30:05 +0300

    taskB-clear

--- src/taskB.js
+++ src/taskB.js
@@ -8,44 +8,43 @@ function summary(req, res) {
   let URIpath = baseURI.path;
 
   let fullname = URIquery.fullname;
-  console.log(`fullname`, fullname);
 
   let fullnameSplit = fullname.split(' ').reverse();
-  console.log(`fullnameSplit`, fullnameSplit);
-  console.log(`fullnameSplit.length`, fullnameSplit.length);
 
 
   if (fullnameSplit.length > 3) {
+
       res.writeHead(200, {'Content-type' : 'text/html'});
       res.end(`<p>Query: /?fullname=${fullname}</p><p>Invalid fullname</p>`);
+
   } else if (fullnameSplit.length === 1) {
+
       let lastName = fullnameSplit[0];
-      console.log(`lastName`, lastName);
       res.writeHead(200, {'Content-type' : 'text/html'});
       res.end(`<p>Query: /?fullname=${fullname}</p><p>${lastName}</p>`);
+
   } else if (fullnameSplit.length === 2) {
+
       let lastName = fullnameSplit[0];
-      console.log(`lastName`, lastName);
       let name = fullnameSplit[1];
       let name1Symbol = name.split(``)[0];
-      console.log(`name`, name);
 
       res.writeHead(200, {'Content-type' : 'text/html'});
       res.end(`<p>Query: /?fullname=${fullname}</p><p>${lastName} ${name1Symbol}.</p>`);
+
   } else {
+
       let name = fullnameSplit[2];
       let name1Symbol = name.split(``)[0];
-      console.log(`name`, name);
 
       let middleName = fullnameSplit[1];
       let middleName1Symbol = middleName.split(``)[0];
-      console.log(`middleName`, middleName);
 
       let lastName = fullnameSplit[0];
-      console.log(`lastName`, lastName);
 
       res.writeHead(200, {'Content-type' : 'text/html'});
       res.end(`<p>Query: /?fullname=${fullname}</p><p>${lastName} ${name1Symbol}. ${middleName1Symbol}.</p>`);
+
   }
 
 }
